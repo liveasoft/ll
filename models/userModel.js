098a419208656c8f68d3aec73efb675a5ac198bf
Zaryab <mzaryabuddin@gmail.com>
2023-06-08 17:21:41 +0500

    Default status changed

--- models/userModel.js
+++ models/userModel.js
@@ -4,7 +4,7 @@ const userSchema = mongoose.Schema({
     name : { type : String, required : [true, "Please enter your name"], trim: true },
     email : { type : String, required : [true, "Please enter your email"], trim: true, unique: true },
     password : { type : String, required : [true, "Please enter your password"], trim: true },
-    status : { type : Number, default : 0 },
+    status : { type : Number, default : 1 },
     avatar : { type : String, default : "https://www.kindpng.com/picc/m/24-248729_stockvader-predicted-adig-user-profile-image-png-transparent.png" },
 },{
     timestamps : true
